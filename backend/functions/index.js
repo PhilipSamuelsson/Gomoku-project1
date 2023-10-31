3ea905215f73438950937362fb9e41aef83fb410
PhilipSamuelsson <philip.samuelsson@iths.se>
2023-10-31 15:05:40 +0100

    Merge branch 'main' of https://github.com/

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -3,114 +3,71 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const admin = require('firebase-admin')
+admin.initializeApp()
+
 const app = express()
 
 app.use(cors({ origin: 'https://starwars-gomoku.web.app' }))
 app.use(express.static('public'))
 app.use(bodyParser.json())
 
-const game = {
-    board: [...Array(15)].map(() => Array(15).fill(null)),
-    currentPlayer: 'player1'
-}
+const db = admin.database()
+const gameRef = db.ref('games/gameID') // Update with your Firebase Realtime Database reference
 
-/**
- * Places the player's stone on the board and switches to the other player.
- * @param {number} row - The row index.
- * @param {number} col - The column index.
- * @param {string} player - The player ('player1' or 'player2').
- */
-// function makeMove(row, col, player) {
-//   game.board[row][col] = player;
-//   game.currentPlayer = player === "player1" ? "player2" : "player1";
-// }
-
-app.get('/', (req, res) => {
-    res.send('tjoooo allihopa')
-})
-
+// Replace your existing code for getting and setting game data
 app.get('/api/get-board', (req, res) => {
-    res.json({ board: game.board, currentPlayer: game.currentPlayer })
-})
-
-app.post('/api/reset-game', (req, res) => {
-    game.board = [...Array(15)].map(() => Array(15).fill(null))
-    game.currentPlayer = 'player1'
-    res.status(200).send('Game reset successfully')
+    gameRef.once('value', (snapshot) => {
+        const gameData = snapshot.val()
+        res.json({
+            board: gameData.board,
+            currentPlayer: gameData.currentPlayer
+        })
+    })
 })
 
 app.post('/api/make-move', (req, res) => {
     const { row, col, player } = req.body
-    console.log('Received move data from client:', { row, col, player })
 
-    game.board[row][col] = player
-    game.currentPlayer = player === 'player1' ? 'player2' : 'player1' // Added
+    // Update the game data in Firebase Realtime Database
+    gameRef.transaction(
+        (currentData) => {
+            if (currentData) {
+                currentData.board[row][col] = player
+                currentData.currentPlayer =
+                    player === 'player1' ? 'player2' : 'player1'
+            }
+            return currentData
+        },
+        (error, committed, snapshot) => {
+            if (error) {
+                console.error('Transaction failed: ', error)
+                res.status(500).send('Move failed')
+            } else {
+                console.log('Transaction completed successfully.')
+                res.status(200).send('Move successful')
+            }
+        }
+    )
+})
 
-    console.log('Updated game board:', game.board)
-    console.log('Next player:', game.currentPlayer)
-
-    res.status(200).send('Move successful')
+app.post('/api/reset-game', (req, res) => {
+    // Reset the game data in Firebase Realtime Database
+    gameRef.set(
+        {
+            board: [...Array(15)].map(() => Array(15).fill(0)),
+            currentPlayer: 'player1'
+        },
+        (error) => {
+            if (error) {
+                console.error('Reset game failed: ', error)
+                res.status(500).send('Reset game failed')
+            } else {
+                console.log('Game reset successfully.')
+                res.status(200).send('Game reset successfully')
+            }
+        }
+    )
 })
 
 exports.app = functions.https.onRequest(app)
-
-// GAMMLA KODEN
-
-// const express = require('express')
-// const app = express()
-// const http = require('http').createServer(app)
-// const bodyParser = require('body-parser')
-// const cors = require('cors')
-
-// app.use(cors({ origin: 'https://starwars-gomoku.web.app/' }))
-// app.use(express.static('public'))
-// app.use(bodyParser.json())
-
-// const game = {
-//     board: [...Array(15)].map(() => Array(15).fill(null)),
-//     currentPlayer: 'player1' // Initialize the current player
-// }
-
-// function makeMove(row, col, player) {
-//     // Place the player's stone on the board
-//     game.board[row][col] = player
-
-//     // Switch to the other player
-//     game.currentPlayer = player === 'player1' ? 'player2' : 'player1'
-// }
-
-// app.get('/', (req, res) => {
-//     res.send('tjoooo allihopa')
-// })
-// // HTTP endpoint for fetching the game board
-// app.get('/api/get-board', (req, res) => {
-//     res.json({ board: game.board, currentPlayer: game.currentPlayer })
-// })
-
-// app.post('/api/reset-game', (req, res) => {
-//     game.board = [...Array(15)].map(() => Array(15).fill(null))
-//     game.currentPlayer = 'player1'
-//     res.status(200).send('Game reset successfully')
-// })
-
-// // HTTP endpoint for handling player moves
-// app.post('/api/make-move', (req, res) => {
-//     const { row, col, player } = req.body
-//     console.log('Received move data from client:', { row, col, player })
-
-//     // Update the game board with the move
-//     game.board[row][col] = player
-
-//     // Log the updated game board
-//     console.log('Updated game board:', game.board)
-
-//     // Log the next player (you should have logic here to switch players)
-//     console.log('Next player:', game.currentPlayer)
-
-//     res.status(200).send('Move successful')
-// })
-
-// const PORT = process.env.PORT || 8000
-// http.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`)
-// })
