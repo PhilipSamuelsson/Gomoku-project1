e515d8cf03c5bdbf655030d47b01a40f523e7d65
MartinITHS <martin@emsound.se>
2023-10-31 10:28:35 +0100

    Update
    Navbar, Playercard, Gomoku Board

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 
 import './styles.css'
-
+import Player from './components/Player'
 import Game from './components/Game'
+import Navbar from './components/Navbar'
+
 
 export default function App() {
     return (
         <div className="App">
+            <Navbar />
+            <Player />
             <Game />
             {/* Render the Game component here */}
             {/* Add other components or content as needed */}
