e07a3c235166dc59b44c12db6a8cd56d400c9318
PhilipSamuelsson <philip.samuelsson@iths.se>
2023-11-06 09:27:12 +0100

    123

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,10 @@
-
 import './styles.css'
-/* import Player from './components/Player' */
+
 import Game from './components/Game'
-/* import Navbar from './components/Navbar' */
-
-
 
 export default function App() {
     return (
         <div className="App">
-{/*             <Navbar />
-            <Player /> */}
             <Game />
             {/* Render the Game component here */}
             {/* Add other components or content as needed */}
