7c2657e2cca15d9ec2d8278f910bd55e7bf07c92
Oskarnn <nygren.a.oskar@gmail.com>
2023-11-07 11:45:03 +0100

    css fix

--- client/src/components/ReadyButton/ReadyButton.jsx
+++ client/src/components/ReadyButton/ReadyButton.jsx
@@ -4,18 +4,25 @@ import "./readyButton.css";
 
 export default function ReadyButton({ title }) {
   const [isActive, setIsActive] = useState(true);
+
+  const onClick = () => {
+    setIsActive(false);
+    setTimeout( () => {alert("Match funnen!")}, 4000)
+
+  };
+
   return (
-    <div
-      className="ready-button-content"
-      onClick={() => setIsActive(false)}
-    >
+    <div className="ready-button-content" onClick={onClick}>
       {isActive ? (
         <div className="ready-button-active">
           <p>{title}</p>
         </div>
       ) : (
         <div className="ready-button-inactive">
-          <p>Finding an opponent... </p>
+          <p>Finding an opponent </p>
+          <div class="dot"></div>
+          <div class="dot"></div>
+          <div class="dot"></div>
         </div>
       )}
     </div>
