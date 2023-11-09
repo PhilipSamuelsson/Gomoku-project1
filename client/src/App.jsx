e424ec856ce348e17278af760a95a96625ecc646
MartinITHS <martin@emsound.se>
2023-11-09 09:45:31 +0100

    Uppdatering
    CSS, RESETBUTTON ETC

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './styles.css'
+<<<<<<< Updated upstream
 import Player from './components/Player'
 import Game from './components/Game'
 import Navbar from './components/Navbar'
@@ -13,4 +14,43 @@ export default function App() {
             {/* Add other components or content as needed */}
         </div>
     )
+=======
+import { useState } from 'react';
+import Player from './components/Player';
+import Game from './components/Game';
+import LandingPage from './components/LandingPage/LandingPage';
+import BackgroundChanger from "./components/BackgroundChanger";
+import Rules from "./components/Rules/Rules";
+import RestartButton from "./components/RestartButton";
+
+export default function App() {
+  const [gameStarted, setGameStarted] = useState(false);
+
+  // Function to start the game when the Ready button is clicked
+  const startGame = () => {
+    setGameStarted(true);
+  };
+
+  return (
+    <div className="App">
+      {gameStarted ? (
+        // Render the game components when the game has started
+        <div>
+          <BackgroundChanger />
+          <section className="player-section">
+            <Player playerNumber={1} />
+            <Player playerNumber={2} />
+          </section>
+          <Game>  <Rules /> </Game>
+        </div>
+      ) : (
+        // Render the LandingPage component until the Ready button is clicked
+        <div>
+
+          <LandingPage onReadyButtonClick={startGame} />
+        </div>)
+      }
+    </div>
+  );
+>>>>>>> Stashed changes
 }
--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -1,5 +1,13 @@
 
+<<<<<<< Updated upstream
 import '../player.css'
+=======
+
+function Player({ playerNumber }) {
+  const [isCharacterSelectionOpen, setCharacterSelectionOpen] = useState(false);
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+  const [playerName, setPlayerName] = useState(`Player ${playerNumber}`);
+>>>>>>> Stashed changes
 
 function Player() {
     return (
@@ -23,6 +31,7 @@ function Player() {
                     </div>
                 </div>
 
+<<<<<<< Updated upstream
                 <div className="playerTwo">
                     <div className="rightContainer">
                         <div className="profileCard">
@@ -40,6 +49,26 @@ function Player() {
                         </div>
                     </div>
                 </div>
+=======
+  const selectCharacter = (character) => {
+    setSelectedCharacter(character);
+    setPlayerName(character.name);
+    toggleCharacterSelection();
+  };
+
+  const characterImageSrc = selectedCharacter ? selectedCharacter.image : '';
+
+  return (
+    <div className="playerContainer" onClick={toggleCharacterSelection}>
+      <div className={`player${playerNumber}`}>
+        <div className="leftContainer">
+          <div className="topLeft">
+            <div className="textTop">{playerName}</div>
+          </div>
+          <div className="botLeft">
+            <div className="timer">
+              <div id={`timerDisplay${playerNumber}`}>00:00</div>
+>>>>>>> Stashed changes
             </div>
 
             {/* <div id="gameContainer">
@@ -47,12 +76,55 @@ function Player() {
                 <button onClick={toggleTimer}>Toggle Timer</button>
             </div> */}
         </div>
+<<<<<<< Updated upstream
     );
 }
 
 let players = [
     { timer: 300 }, // Player One with 5 minutes
     { timer: 300 }  // Player Two with 5 minutes
+=======
+        <div className="rightContainer">
+          <div className="profileCard">
+            <img
+              className="profileImage"
+              src={characterImageSrc}
+              alt="Profile Image"
+            />
+          </div>{/* ... (Player's rightContainer content) */}
+        </div>
+      </div>
+      <div className={`characterSelection ${isCharacterSelectionOpen ? 'open' : ''}`}>
+        <CharacterSelection
+          isOpen={isCharacterSelectionOpen}
+          onClose={toggleCharacterSelection}
+          characters={starWarsCharacters}
+          onSelectCharacter={selectCharacter}
+          playerNumber={playerNumber}
+        />
+      </div>
+    </div>
+  );
+}
+
+Player.propTypes = {
+  playerNumber: PropTypes.number.isRequired,
+
+};
+
+const starWarsCharacters = [
+  {
+    name: 'Luke Skywalker',
+    image: 'https://i.pinimg.com/originals/4c/e3/44/4ce3446ef8dbbd5d9feaedb8caadc897.png',
+  },
+  {
+    name: 'Princess Leia',
+    image: 'https://i.pinimg.com/originals/be/ae/49/beae4969d8151ee3405b9b8131e2081f.png',
+  }, {
+    name: 'Darth Vader',
+    image: 'https://i.pinimg.com/originals/9f/9f/73/9f9f73b4b9b89101f9419241d64233d3.png'
+  }
+>>>>>>> Stashed changes
 ];
 let currentPlayerIndex = 0;
 let interval;
